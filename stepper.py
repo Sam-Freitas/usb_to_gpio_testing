44a3952a42857b5708009c0ed05f9c7c2f3aeac7
Sam-Freitas <samfreitas@email.arizona.edu>
2022-06-02 16:13:48 -0700

    Update stepper.py

--- stepper.py
+++ stepper.py
@@ -1,20 +1,32 @@
+# in powershell $env:BLINKA_MCP2221=1
+# in cmd set BLINKA_MCP2221=1
+# import os
+# os.system('set BLINKA_MCP2221=1')
+# os.system('$env:BLINKA_MCP2221=1')
+
+# sometime the I2C ports might get messed up. unplug everything from the breadboard and wait a minute then plug back in
+
 import time
 import board
 print(dir(board))
 import digitalio
 from numpy import invert
+from threading import Event
+from random import randint
 
-def step(n, direction, dir_pin, step_pin, step_delay = 5000/(10**6)):
+def step(n, direction, dir_pin, step_pin, step_delay = 0):
 
     step_pin.value = False
+    dir_pin.value = direction
 
     for i in range(n):
         step_pin.value = True
-        dir_pin.value = direction
-
-        time.sleep(10/(10**6))
+        # Event().wait(0.05)
+        # Event().wait(0.0001)
+        # time.sleep(10/(10**6))
         step_pin.value = False
-        time.sleep(step_delay)
+        Event().wait(step_delay)
+        # time.sleep(step_delay)
         pass
 
 
@@ -27,5 +39,7 @@ step_pin.direction = digitalio.Direction.OUTPUT
 direction = False
 while True:
     direction = invert(direction)
-    print(direction)
-    step(200, direction, dir_pin, step_pin)
+    Event().wait(0.5)
+    n_steps = randint(1,200)
+    print(direction,n_steps)
+    step(n_steps, direction, dir_pin, step_pin)
